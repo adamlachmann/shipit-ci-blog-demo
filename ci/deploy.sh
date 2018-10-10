75a2b5d04c7adb59db88bc2d1046efce7b930d9f
Adam Lachmann <adam.lachman@rst-it.com>
2018-10-10 03:17:58 +0200

    feat: encrypted codeship ssh key, added to docker image and deploy script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# SSH key config
+echo -e $PRIVATE_SSH_KEY >> /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
+ssh-keyscan -H $SERVER_IP >> /root/.ssh/known_hosts
+
 # Shipit.js
 npx shipit vps deploy --shipitfile ci/shipitfile.js
